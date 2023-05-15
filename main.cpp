42ead2c6ea66980c77c29395b723e995e1e73dc6
EGheska <dimanich30@gmail.com>
2023-05-15 21:46:27 +0200

    pacanov vse ecsche ebut na plusah...

--- main.cpp
+++ main.cpp
@@ -44,30 +44,55 @@ int symbolCount(std::string filename){
     return count;
 }
 
+int longestLine(std::string filename){
+    int size = 0;
+    string line;
+    ifstream file(filename);
+    while(getline(file, line)){
+        int lineSize =0;
+        for(char c : line){
+            if (c !='\n' && c != '\r'){
+                lineSize++;
+            }
+        }
+        if (lineSize > size){
+            size = lineSize;
+        }
+    }
+    return size;
+}
+
 int main(int argc, char** argv)
 {
     int words = 0;
     int lines = 0;
     int symbols = 0;
-    char ch;
-    size_t longestLine = 0;
-    if (!argv[1]) { // pacanov
-        throw std::invalid_argument("Invalid argument");
-    }
-    if (argv[2]) { // ebut
+    int longLine = 0;
+    if (argc == 1) {
         words = wordCount("anna_karenina.txt");
-    }
-    if (argv[3]) { // na
+        cout << "Number of words in text: "<< words << "\n";
         lines = lineCount("anna_karenina.txt");
-    }
-    if (argv[4]) { // plusah
+        cout << "Number of lines in text: " << lines-1 << "\n";
         symbols = symbolCount("anna_karenina.txt");
+        cout << "Number of symbols in text: " << symbols << "\n";
     }
-    if (argv[5]) { // does not exist
-
+    if (argc > 1){ // pacanov
+        // TODO: size of line is 1 instead of the real size
+        longLine = longestLine("anna_karenina.txt");
+        cout << "Size of the longest line in text: " << longestLine << "\n";
     }
-    cout << "Number of symbols in text: " << symbols << "\n";
-    cout << "Number of lines in text: " << lines-1 << "\n";
-    cout << "Number of words in text: "<< words;
+    if (argc > 2) { // ebut
+        words = wordCount("anna_karenina.txt");
+        cout << "Number of words in text: "<< words << "\n";
+    }
+    if (argc > 3) { // na
+        lines = lineCount("anna_karenina.txt");
+        cout << "Number of lines in text: " << lines-1 << "\n";
+    }
+    if (argc > 4) { // plusah
+        // TODO: count of symbols is not correct
+        symbols = symbolCount("anna_karenina.txt");
+        cout << "Number of symbols in text: " << symbols << "\n";
+    }
     return 0;
 }
